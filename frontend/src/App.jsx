af2a0269e42e6282093a456e7d63f8a2771c699c
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-24 01:04:43 +0530

    login page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './Pages/Login'
+import Feed from './Pages/Feed'
+import Messages from './Pages/Messages'
+import ChatBox from './Pages/ChatBox'
+import Connection from './Pages/Connection'
+import Discover from './Pages/Discover'
+import Profile from './Pages/Profile'
+import CreatePost from './Pages/CreatePost'
 
 function App() {
 
   return (
     <>
-    <div>hhh</div>
+      <Routes>
+        <Route path='/' element={<Login />} >
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='messages/:userId' element={<ChatBox />} />
+          <Route path='connections' element={<Connection />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile/:profileId' element={<Profile />} />
+          <Route path='create-post' element={<CreatePost />} />
+        </Route>
+
+      </Routes>
     </>
   )
 }
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <section className='min-h-screen'>
+      {/* bg-image */}
+      <img src={assets.bgImage} alt="graident bgImage"
+        className='absolute top-0 left-0 -z-1 w-full h-full object-cover'
+      />
+
+      {/* left side : branding */}
+      <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
+        <img
+          src={assets.logo}
+          alt="logo image"
+          className='h-10 object-contain'
+        />
+        <div>
+          <div className='flex items-center gap-3 mb-4 max-md:mt-10'>
+            <img src={assets.group_users} alt="group users image"
+              className='h-8 md:h-10'
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
