678c465ef32c5eb17d032400c77b118a499ec293
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-27 00:01:44 +0530

    sidebar created

--- frontend/src/Pages/Layout.jsx
+++ frontend/src/Pages/Layout.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../Components/Sidebar'
+import { Outlet } from 'react-router-dom'
+import { Menu, X } from 'lucide-react'
+import { dummyUserData } from '../assets/assets'
 
 function Layout() {
-  return (
-    <div>
-      Layout
-    </div>
+  const user = dummyUserData
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  return user ? (
+    <section className='w-full flex h-screen'>
+      <Sidebar />
+      <div className='flex-1 bg-slate-50'>
+        <Outlet />
+      </div>
+      {
+        sidebarOpen ?
+          <X className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={() => setSidebarOpen(false)} /> : <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={() => setSidebarOpen(true)} />
+      }
+    </section>
+  ) : (
+    <h1>Loading</h1>
   )
 }
 
