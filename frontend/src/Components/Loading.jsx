fddfe5e705643a79fd16dd47d50b16c7c0a10bc5
Sanjyrana <Sanjayrana5113@gmail.com>
2025-09-28 23:11:07 +0530

    modification in feed

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Loading({ height = "100vh" }) {
+function Loading() {
     return (
         <div className='flex items-center justify-center h-screen'>
-            <div style={{ height }} className='w-10 h-10 rounded-full border-3 border-purple-500 border-t-transparent animate-spin'>
+            <div className='w-10 h-10 rounded-full border-3 border-purple-500 border-t-transparent animate-spin'>
 
             </div>
         </div>
--- frontend/src/Pages/Feed.jsx
+++ frontend/src/Pages/Feed.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../Components/Loading'
 
 function Feed() {
-  return (
-    <div>
-      Feed
+  const [feeds, setFeeds] = useState([])
+  const [loading,setLoading]=useState(true)
+
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchFeeds()
+  }, [])
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scroolbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* stories and post list */}
+      <div>
+        <h1>Stories here</h1>
+        <div className='p-4 space-y-6'>
+          List of post
+        </div>
+      </div>
+      {/* right sidebar */}
+      <div>
+        <div>
+          <h1>Sponsered</h1>
+          <h1>
+            Recent messages
+          </h1>
+        </div>
+      </div>
     </div>
-  )
+  ) : <Loading height=""/>
 }
 
 export default Feed
