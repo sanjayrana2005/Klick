43f60446324f48697e9b3845803df6c02a07bc4a
Sanjyrana <Sanjayrana5113@gmail.com>
2025-10-07 23:04:39 +0530

    like share comment features added

--- frontend/src/Components/PostCard.jsx
+++ frontend/src/Components/PostCard.jsx
@@ -1,8 +1,17 @@
-import { BadgeCheck } from 'lucide-react'
-import React from 'react'
+import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react'
+import React, { useState } from 'react'
 import moment from "moment"
+import { dummyUserData } from '../assets/assets'
 
 const PostCard = ({ post }) => {
+    const [likes, setLikes] = useState(post.likes_count)
+    const currentUser = dummyUserData
+
+    const postWithHastags = post.content.replace(/(#\w+)/g, "<span class='text-indigo-600'>$1</span>")
+
+    const handleLikes = async () => {
+
+    }
     return (
         <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full'>
             {/* user info */}
@@ -20,15 +29,34 @@ const PostCard = ({ post }) => {
             </div>
 
             {/* Content */}
-            {post.content && <div className='text-gray-800 text-sm whitespace-pre-line' dangerouslySetInnerHTML={{__html:post.content}}/>}
+            {post.content && <div className='text-gray-800 text-sm whitespace-pre-line' dangerouslySetInnerHTML={{ __html: postWithHastags }} />}
 
             {/* images */}
             <div className='grid grid-cols-2 gap-2'>
-{
-    post.image_urls.map((img,index)=>{
-        return <img src={img} className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`}/>
-    })
-}
+                {
+                    post.image_urls.map((img, index) => {
+                        return <img src={img} className={`w-full h-48 object-cover rounded-lg ${post.image_urls.length === 1 && 'col-span-2 h-auto'}`} />
+                    })
+                }
+            </div>
+
+
+            {/* Acttions */}
+            <div className='flex items-center gap-4 text-gray-600 text-sm pt-2 border-t border-gray-300'>
+                <div className='flex items-center gap-1'>
+                    <Heart className={`w-4 h-4 cursor-pointer ${likes.includes(currentUser.id) && 'text-red-500'}`} onClick={handleLikes} />
+                    <span>{likes.length}</span>
+                </div>
+
+                <div className='flex items-center gap-1'>
+                    <MessageCircle className="w-4 h-4" />
+                    <span>{12}</span>
+                </div>
+
+                <div className='flex items-center gap-1'>
+                    <Share2 className="w-4 h-4" />
+                    <span>{7}</span>
+                </div>
             </div>
         </div>
     )
