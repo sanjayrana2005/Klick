01f4a2adab66baeb530e2240196a45037c3d1711
Sanjyrana <Sanjayrana5113@gmail.com>
2025-10-07 23:33:09 +0530

    sponsered section modified

--- frontend/src/Pages/Feed.jsx
+++ frontend/src/Pages/Feed.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../Components/Loading'
 import Storiesbar from '../Components/Soriesbar'
 import PostCard from '../Components/PostCard'
@@ -19,7 +19,7 @@ function Feed() {
   return !loading ? (
     <div className='h-full overflow-y-scroll no-scroolbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
       {/* stories and post list */}
-      <div>
+      <div className="flex-1 max-w-[650px]">
         <Storiesbar/>
         <div className='p-4 space-y-6'>
           {feeds.map((post,index)=>(
@@ -28,9 +28,12 @@ function Feed() {
         </div>
       </div>
       {/* right sidebar */}
-      <div>
-        <div>
-          <h1>Sponsered</h1>
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsered</h3>
+          <img src={assets.sponsored_img} alt="sponsered image" className='w-75 h-50 rounded-md' />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful,easy-to-use platform built for results.</p>
           <h1>
             Recent messages
           </h1>
